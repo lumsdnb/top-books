f247c05cbe6a147ba26b396d44184971e872e6ba
Jeremu <jeremy@lums.io>
2021-04-21 11:43:42 +0200

    Add 2 ways of creating books

---
+++ books.js
@@ -0,0 +1,35 @@
+//simple
+
+const Books = function (title, author, pages, wasRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.wasRead = wasRead;
+
+  this.info = () => {
+    return `${this.title} by ${this.author}, ${this.pages} pages ${
+      this.wasRead ? "has been read already" : "not read yet"
+    }`;
+  };
+};
+
+const aBook = Object.create(Books("a title", "an author", 200, false));
+
+//this also works
+
+const Books2 = {
+  init: function (title, author, pages, wasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.wasRead = wasRead;
+  },
+
+  info: () => {
+    return `${this.title} by ${this.author}, ${this.pages} pages ${
+      this.wasRead ? "has been read already" : "not read yet"
+    }`;
+  },
+};
+
+const anotherBook = Object.create(Books2).init("tit", "auth", "22", true);
