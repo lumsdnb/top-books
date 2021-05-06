e9d3d3062a856cd3cf75e4f52041d8b2c28c0029
Jeremu <jeremy@lums.io>
2021-05-06 11:36:51 +0200

    Add array & function, prettify

--- books.js
+++ books.js
@@ -8,12 +8,12 @@ const Books = function (title, author, pages, wasRead) {
 
   this.info = () => {
     return `${this.title} by ${this.author}, ${this.pages} pages ${
-      this.wasRead ? "has been read already" : "not read yet"
+      this.wasRead ? 'has been read already' : 'not read yet'
     }`;
   };
 };
 
-const aBook = Object.create(Books("a title", "an author", 200, false));
+const aBook = Object.create(Books('a title', 'an author', 200, false));
 
 //this also works
 
@@ -27,9 +27,18 @@ const Books2 = {
 
   info: () => {
     return `${this.title} by ${this.author}, ${this.pages} pages ${
-      this.wasRead ? "has been read already" : "not read yet"
+      this.wasRead ? 'has been read already' : 'not read yet'
     }`;
   },
 };
 
-const anotherBook = Object.create(Books2).init("tit", "auth", "22", true);
+const anotherBook = Object.create(Books2).init('tit', 'auth', '22', true);
+
+let myLibrary = [
+  { title: 'a book title', author: 'jk rolling', pages: 5, wasRead: true },
+  { title: 'another one', author: 'hp lowcraft', pages: 666, wasRead: false },
+];
+
+function addBookToLibrary() {
+  //yo
+}
